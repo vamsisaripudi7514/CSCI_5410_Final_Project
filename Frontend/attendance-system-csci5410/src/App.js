2f1c6692b4adab43d7c6a51973c6dcc84356a392
jaswanthbondalapati <jb2hs@mtmail.mtsu.edu>
2025-04-26 16:02:49 -0500

    CLASS MANAGEMENT

--- Frontend/attendance-system-csci5410/src/App.js
+++ Frontend/attendance-system-csci5410/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Studentdashboard from './student/sdashboard';
 import Teacherdashboard from './teacher/tdashboard';
+import Classdashboard  from './class_management/class_dashboard';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Route path="/" element={<Navigate to="/teacher" />} /> 
         <Route path="/student" element={<Studentdashboard />} />
         <Route path="/teacher" element={<Teacherdashboard />} />
-        
+        <Route path="/classmanagement" element={<Classdashboard/>} />
       </Routes>
     </BrowserRouter>
   );
